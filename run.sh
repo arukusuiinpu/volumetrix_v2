b973ae9fc66450ad2a1d7b51a637373e9ca79f4e
arukusu iinpu <Alex.Nompe@yandex.ru>
2026-04-09 14:00:38 +0300

    even more stuff

---
+++ run.sh
@@ -0,0 +1,3 @@
+
+#!/bin/bash
+sudo /opt/volumetrix/rpi-fb-matrix/rpi-rgb-led-matrix/examples-api-use/demo -D0 --led-rows=32 --led-cols=64 --led-pwm-bits=8 --led-pwm-lsb-nanoseconds=50 --led-rgb-sequence="rbg" -R 90

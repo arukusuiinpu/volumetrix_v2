338a350b0de590a56d838a99ff206f93603e10f8
arukusu iinpu <Alex.Nompe@yandex.ru>
2026-04-17 11:17:04 +0300

    Update demo-main.cc

--- rpi-fb-matrix/rpi-rgb-led-matrix/examples-api-use/demo-main.cc
+++ rpi-fb-matrix/rpi-rgb-led-matrix/examples-api-use/demo-main.cc
@@ -144,6 +144,10 @@ struct Vector3 {
     bool is_initialized() const {
         return !std::isnan(x);
     }
+
+    double magnitude() {
+        return x * x + y * y + z * z;
+    }
 };
 
 struct Vector2 {
@@ -503,7 +507,7 @@ public:
                   ((float)rawGZ / GYRO_SENSITIVITY_250DPS) * (M_PI / 180.0f),
                 };
 
-                std::cout << "Angles: " << raw_gyro.x << ", " << raw_gyro.y << ", " << raw_gyro.z << std::endl;
+                std::cout << "Gyro: " << raw_gyro.magnitude() << ", " << raw_gyro.x << ", " << raw_gyro.y << ", " << raw_gyro.z << std::endl;
 
                 const float GYRO_DEADZONE = 0.05f;
 

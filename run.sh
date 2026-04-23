f39fe64e346f3c750276ab23d5498c17afe6dbfe
arukusu iinpu <95543518+arukusuiinpu@users.noreply.github.com>
2026-04-23 10:07:09 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 
 #!/bin/bash
+sudo chmod +x /opt/volumetrix/rpi-fb-matrix/rpi-rgb-led-matrix/examples-api-use/demo
 sudo /opt/volumetrix/rpi-fb-matrix/rpi-rgb-led-matrix/examples-api-use/demo -D0 --led-rows=32 --led-cols=64 --led-pwm-bits=8 --led-pwm-lsb-nanoseconds=50 --led-rgb-sequence="rbg" -R 90
